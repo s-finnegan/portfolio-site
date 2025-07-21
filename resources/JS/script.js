06d1e5c6aa2722e939c4316c1caafd46330048d9
Shaun-Finnegan <sfinnegan.dev@gmail.com>
2025-07-21 12:57:36 +0100

    Have added basic animation to the canvas hero image

--- resources/JS/script.js
+++ resources/JS/script.js
@@ -1,4 +1,39 @@
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
-ctx.fillStyle = 'white';
-ctx.fillRect(0, 0, 50, 50);
+canvas.width = window.innerWidth;
+canvas.height = 400;
+
+window.addEventListener('resize', function(){
+canvas.width = window.innerWidth;
+canvas.height = 400;
+});
+
+const mouse = {
+    x: undefined,
+    y: undefined,
+}
+
+canvas.addEventListener('click', function(event){
+    mouse.x = event.x;
+    mouse.y = event.y;
+    drawCircle();
+    
+});
+
+function drawCircle(){
+   ctx.fillStyle = 'red';
+    ctx.strokeStyle = 'red';
+    ctx.beginPath();
+    ctx.arc(mouse.x, mouse.y, 50, 0, Math.PI * 2);
+    ctx.stroke();
+     ctx.fill()
+};
+
+function animate(){
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawCircle();
+  requestAnimationFrame(animate);
+};
+
+animate();
+
